e9b8b50fa18cfecfcdfe7b94ac4b682742db6f9e
Stephanie Galvan <55038687+stephypy@users.noreply.github.com>
2020-09-28 20:04:06 -0600

    first draft for server and client

--- file-transfer-lab/fileClient.py
+++ file-transfer-lab/fileClient.py
@@ -1,1 +1,46 @@
 #!/usr/bin/env python3
+
+import socket, sys, re
+
+sys.path.append("../lib")  # for params
+import params
+
+
+def client():
+    switchesVarDefaults = (
+        (('-s', '--server'), 'server', "127.0.0.1:50001"),
+        (('-d', '--debug'), "debug", False),  # boolean (set if present)
+        (('-?', '--usage'), "usage", False),  # boolean (set if present)
+    )
+
+    paramMap = params.parseParams(switchesVarDefaults)
+
+    server, usage, debug = paramMap["server"], paramMap["usage"], paramMap["debug"]
+
+    if usage:
+        params.usage()
+
+    try:
+        serverHost, serverPort = re.split(":", server)
+        serverPort = int(serverPort)
+    except:
+        print("Can't parse server:port from '%s'" % server)
+        sys.exit(1)
+
+    addr_port = (serverHost, serverPort)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect(addr_port)
+
+        while True:
+            filename = input("> ")
+            if filename == "exit":
+                sys.exit(0)
+            else:
+                s.sendall(filename.encode())
+                data = s.recv(1024)
+                print('Received', repr(data))
+
+
+if __name__ == "__main__":
+    client()
--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -1,1 +1,49 @@
 #!/usr/bin/env python3
+
+import socket, sys
+
+sys.path.append("../lib")  # for params
+import params
+
+HOST = "127.0.0.1"
+
+
+def server():
+    switchesVarDefaults = (
+        (('-l', '--listenPort'), 'listenPort', 50001),
+        (('-d', '--debug'), "debug", False),  # boolean (set if present)
+        (('-?', '--usage'), "usage", False),  # boolean (set if present)
+    )
+
+    paramMap = params.parseParams(switchesVarDefaults)
+    debug, listenPort = paramMap['debug'], paramMap['listenPort']
+
+    if paramMap['usage']:
+        params.usage()
+
+    bind_addr = (HOST, listenPort)
+
+    # creating listening socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # associating socket with host and port number
+        s.bind(bind_addr)
+
+        # "makes" s listening socket
+        s.listen()
+        print("listening on: ", bind_addr)
+
+        # connection and tuple for client address (host, port)
+        conn, addr = s.accept()
+
+        with conn:
+            print("connection rec'd from", addr)
+            data = conn.recv(1024)
+            decoded_data = data.decode()
+            while True:
+                if not data:
+                    break
+                conn.sendall(data)
+
+
+if __name__ == "__main__":
+    server()
